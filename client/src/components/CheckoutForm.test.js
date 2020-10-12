068611ca3a27ba69335fd8ff2cfb1c0c462a9fe9
matieuruth <matieu.ruth@gmail.com>
2020-10-11 20:38:51 -0400

    attempted testing

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,21 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen, getByAltText } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+
+//comps
+import App from '../App';
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders", () => {
+        //Arrange
+        render(<App/>)
+        //Act
+        screen.getByText(/react plants/)
+
+
+        //Assert
+    
+});
 
 test("form shows success message on submit with form details", () => {});
